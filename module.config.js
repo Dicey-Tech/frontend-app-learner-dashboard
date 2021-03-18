3ff2f483c7ee7ce0160ceab61bb70c9eeb174a8d
Salik Rafiq <chameeyass@hotmail.com>
2021-03-18 14:35:30 +0000

    Add export module.config.js to load in the Dicey Tech components in replacement of the header, footer and logo

---
+++ module.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    localModules: [
+       { moduleName: '@edx/frontend-component-footer', dir: '../components/frontend-component-footer-educate'},
+       { moduleName: '@edx/frontend-component-header', dir: '../components/frontend-component-header-educate'},
+       { moduleName: '@edx/brand-openedx', dir: '../components/brand-openedx'},
+
+          ],
+  };
